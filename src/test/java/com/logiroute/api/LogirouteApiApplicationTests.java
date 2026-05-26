e944d17105dea8273e1603e5be1f700f05aaf1bd
LuizArioza <luizarioza@gmail.com>
2026-05-26 19:15:21 -0300

    feat: estrutura inicial do projeto LogiRoute API

---
+++ src/test/java/com/logiroute/api/LogirouteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logiroute.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogirouteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
